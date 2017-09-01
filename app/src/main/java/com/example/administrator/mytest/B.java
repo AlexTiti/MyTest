f92cd10b29a3c8ce38c4ad77922ad30e44256e13
Administrator <18238818283@sina.cn>
2017-09-01 20:36:01 +0800

    commit_testbrach

---
+++ app/src/main/java/com/example/administrator/mytest/B.java
@@ -0,0 +1,13 @@
+package com.example.administrator.mytest;
+
+/**
+ * <pre>
+ *
+ *   author   :   Alex
+ *   e_mail   :   18238818283@sina.cn
+ *   timr     :   2017/09/01
+ *   desc     :
+ *   version  :   V 1.0.5
+ */
+public class B {
+}
